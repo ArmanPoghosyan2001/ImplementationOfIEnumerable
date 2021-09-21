3b9003b84cf228ef5631dd949d1108481817b67a
ArmanPoghosyan2001 <a.poghosyan3001@gmail.com>
2021-09-21 14:08:29 +0400

    Add project files.

---
+++ Collections/Element.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Element
+    {
+        private string name;
+        private int field1;
+        private int field2;
+
+        public Element(string name, int field1, int field2)
+        {
+            this.name = name;
+            this.field1 = field1;
+            this.field2 = field2;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Field1
+        {
+            get { return field1; }
+            set { field1 = value; }
+        }
+        public int Field2
+        {
+            get { return field2; }
+            set { field2 = value; }
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserCollection users = new UserCollection();
+
+            IEnumerable enumerable = users as IEnumerable;
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Element element = enumerator.Current as Element;
+                Console.WriteLine(element.Name);
+            }
+            enumerator.Reset();
+        }
+    }
+}
